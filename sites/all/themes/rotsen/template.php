ea1f32f3a9029bebae9855eaf1103d9dfcfa4f9b
Kyle Taylor <kyletaylored@gmail.com>
2014-09-15 01:49:32 -0500

    Rotsen updates

--- sites/all/themes/rotsen/template.php
+++ sites/all/themes/rotsen/template.php
@@ -1,6 +1,30 @@
 <?php
 
-/**
- * @file
- * template.php
- */
+function rotsen_form_system_theme_settings_alter(&$form, &$form_state) {
+
+	if (module_exists('colourlovers')) {
+		$form['colourlovers_settings'] = array(
+			'#type' => 'fieldset',
+			'#title' => t('COLOURLovers Settings'),
+			'#weight' => -1,
+		);
+		$form['colourlovers_settings']['colourlovers_check'] = array(
+			'#type' => 'checkbox',
+			'#title' => t('Enable COLOURLovers palettes'),
+			'#default_value' => theme_get_setting('colourlovers_check'),
+			'#description' => t('Checking this will replace the current color schemes with those provided by the !colourlover.', array('!colourlover' => l('COLOURLovers API', 'admin/appearance/colourlovers'))),
+		);
+		$form['colourlovers_settings']['colourlovers_container'] = array(
+			'#type' => 'container',
+	  	'#states' => array(
+				'visible' => array(
+					':input[name="colourlovers_check"]' => array('checked' => TRUE),
+				),
+			),
+		);
+		$form['colourlovers_settings']['colourlovers_container']['colourlovers_demo'] = array(
+			'#type' => 'markup',
+	  	'#markup' => variable_get('colourlovers_demo_html', 'No palettes generated.'),
+	  );
+	}
+}
