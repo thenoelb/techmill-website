bb711c954ddd3f2c63ec96a76a307f28eb74da53
Kyle Taylor <kyletaylored@gmail.com>
2016-06-15 14:50:00 -0500

    Update CKEditor Media

--- sites/all/modules/ckeditor_media/plugins/mediaBrowser/plugin.js
+++ sites/all/modules/ckeditor_media/plugins/mediaBrowser/plugin.js
@@ -1,7 +1,9 @@
 /**
  * @file Plugin for replacing filebrowser dialog with dialog from Drupal media module
  */
-(function($) {
+
+ (function ($) {
+  Drupal.media = Drupal.media || {};
 
   // Traverse through the content definition and attach mediabrowser to
   // elements with 'filebrowser' attribute.
@@ -13,20 +15,66 @@
   // @param {Array}
   //            elements Array of {@link CKEDITOR.dialog.definition.content}
   //            objects.
-  function attachMediaBrowser(editor, dialogName, definition, elements) {
-    if (!elements || !elements.length)
+
+  function mediaBrowser (evt) {
+    var dialog = evt.data.dialog;
+    // Media z-index is 10002 so we need to be below that.
+    var dialogElement = dialog.getElement().getFirst();
+    dialogElement.setStyle('z-index', 10001);
+    // Invoke the media popup for file selection.
+    var mediaIframe = Drupal.media.popups.mediaBrowser(function(mediaFiles) {
+      if (mediaFiles.length > 0) {
+        // There is probably a better way of getting the url for the file
+        // but media returns the object with an absolute url and the uri.
+        // We don't have access to file_create_url in javascript so we can't
+        // use that. For now, just remove the server name from the absolute
+        // url to get the relative. FIX THIS!!!
+        var fileUrl = mediaFiles[0].url.replace(location.origin, '');
+        var fileAlt = (mediaFiles[0].alt) ? mediaFiles[0].alt : "" ;
+        var fileTitle = (mediaFiles[0].title) ? mediaFiles[0].title : "";
+
+        var parts = evt.sender.filebrowser.target.split(':');
+        dialog.setValueOf(parts[0], parts[1], fileUrl);
+
+        // Add title/alt attr using default CKEditor image values.
+        var altEl = dialog.getContentElement("info", "txtAlt");
+        if (altEl) {
+          altEl.setValue(fileAlt);
+        }
+
+        // Try CKEditor defined title value first.
+        var titleEl = dialog.getContentElement("info", "txtGenTitle");
+        if (titleEl) {
+          titleEl.setValue(fileTitle);
+        } else {
+          // If it doesn't work, try a more generic ID.
+          titleEl = dialog.getContentElement("info", "txtTitle");
+          if (titleEl) {
+            titleEl.setValue(fileTitle);
+          }
+        }
+      }
+    });
+    $(mediaIframe).parent().css({'z-index':'10002'});
+  }
+
+  function attachMediaBrowser (editor, dialogName, definition, elements) {
+    if (!elements || !elements.length){
       return;
+    }
 
     var element, fileInput;
 
     for (var i = elements.length; i--; ) {
       element = elements[ i ];
 
-      if (element.type == 'hbox' || element.type == 'vbox' || element.type == 'fieldset')
+      if (element.type == 'hbox' || element.type == 'vbox' || element.type == 'fieldset') {
         attachMediaBrowser(editor, dialogName, definition, element.children);
+      }
 
-      if (!element.filebrowser)
+      if (!element.filebrowser){
         continue;
+      }
 
       if (typeof element.filebrowser == 'string') {
         var fb = {
@@ -48,45 +96,18 @@
     }
   }
 
-  function mediaBrowser(evt) {
-    var dialog = evt.data.dialog;
-    // Media z-index is 10002 so we need to be below that.
-    var dialogElement = dialog.getElement().getFirst();
-    dialogElement.setStyle('z-index', 10001);
-    // Invoke the media popup for file selection.
-    var mediaIframe = Drupal.media.popups.mediaBrowser(function(mediaFiles) {
-      if (mediaFiles.length > 0) {
-        // There is probably a better way of getting the url for the file
-        // but media returns the object with an absolute url and the uri.
-        // We don't have access to file_create_url in javascript so we can't
-        // use that. For now, just remove the server name from the absolute
-        // url to get the relative. FIX THIS!!!
-        var fileUrl = mediaFiles[0].url.replace(location.origin, '');
-        var parts = evt.sender.filebrowser.target.split(':');
-        dialog.setValueOf(parts[0], parts[1], fileUrl);
-      }
-    });
-    $(mediaIframe).parent().css({'z-index':'10002'});
-  }
-
-  CKEDITOR.plugins.add('mediaBrowser',
-          {
-            requires: ['dialog', 'media'],
-            init: function(editor) {
-              // Needed to keep things happy.
-            }
-          });
+  CKEDITOR.plugins.add('mediaBrowser', {
+    requires: ['dialog'],
+    init: function(editor) { /*Needed to keep things happy.*/ }
+  });
 
   CKEDITOR.on('dialogDefinition', function(evt) {
     var definition = evt.data.definition,
-            element;
+    element;
     // Associate mediabrowser to elements with 'filebrowser' attribute.
     for (var i = 0; i < definition.contents.length; ++i) {
       if ((element = definition.contents[ i ])) {
         attachMediaBrowser(evt.editor, evt.data.name, definition, element.elements);
-        if (element.hidden && element.filebrowser && element.type != 'fileButton') {
-          element.hidden = false;
-        }
       }
     }
   });
